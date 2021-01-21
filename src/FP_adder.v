cb099b0a3f210f07bb31ea471d73aadbc5ee7741
ahmadsalimi <ahsa9978@gmail.com>
2021-01-21 16:47:07 +0330

    change adder interface

--- src/FP_adder.v
+++ src/FP_adder.v
@@ -1,29 +1,15 @@
 module adder(
-        input_a,
-        input_b,
-        input_a_stb,
-        input_b_stb,
-        output_z_ack,
-        clk,
-        rst,
-        output_z,
-        output_z_stb,
-        input_a_ack,
-        input_b_ack);
-
-  input     clk;
-  input     rst;
-
-  input     [31:0] input_a;
-  input     input_a_stb;
-  output    input_a_ack;
-
-  input     [31:0] input_b;
-  input     input_b_stb;
-  output    input_b_ack;
-
-  output    [31:0] output_z;
-  output    output_z_stb;
-  input     output_z_ack;
+    input           clk,
+    input           rst,
+    input   [31:0]  a_in,
+    input           a_stb,
+    input   [31:0]  b_in,
+    input           b_stb,
+    input           z_ack,
+    output          a_ack,
+    output          b_ack,
+    output  [31:0]  z_out,
+    output          z_stb
+);
 
 endmodule
