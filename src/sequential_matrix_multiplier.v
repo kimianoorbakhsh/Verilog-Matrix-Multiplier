ccbd0074cbe728f996cc2f65f8b02aad55b285a8
ahmadsalimi <ahsa9978@gmail.com>
2021-01-21 16:36:41 +0330

    fix

--- src/sequential_matrix_multiplier.v
+++ src/sequential_matrix_multiplier.v
@@ -11,7 +11,7 @@ module sequential_matrix_multiplier
     output [$ceil($clog2(m))-1:0]   a_j,
     output [$ceil($clog2(m))-1:0]   b_i,
     output [$ceil($clog2(m))-1:0]   b_j,
-    output [31:0]   output_z,
+    output [31:0]   output_z
 );
 
 endmodule
