34e50a9d80312ed77b0bede8c231427f9d347f1d
ahmadsalimi <ahsa9978@gmail.com>
2021-01-21 16:41:49 +0330

    another change to seq

--- src/sequential_matrix_multiplier.v
+++ src/sequential_matrix_multiplier.v
@@ -3,15 +3,18 @@ module sequential_matrix_multiplier
     parameter m = 4
 )
 (
-    input [31:0]    input_a,
-    input [31:0]    input_b,
-    input           clk,
-    input           rst,
-    output [$ceil($clog2(m))-1:0]   a_i,
-    output [$ceil($clog2(m))-1:0]   a_j,
-    output [$ceil($clog2(m))-1:0]   b_i,
-    output [$ceil($clog2(m))-1:0]   b_j,
-    output [31:0]   output_z
+    input                           clk,
+    input                           rst,
+    input   [31:0]                  a_in,
+    input   [31:0]                  b_in,
+    output  [$ceil($clog2(m))-1:0]  a_i,
+    output  [$ceil($clog2(m))-1:0]  a_j,
+    output  [$ceil($clog2(m))-1:0]  b_i,
+    output  [$ceil($clog2(m))-1:0]  b_j,
+    output  [31:0]                  z_out,
+    output  [$ceil($clog2(m))-1:0]  z_i,
+    output  [$ceil($clog2(m))-1:0]  z_j,
+    output                          z_stb
 );
 
 endmodule
