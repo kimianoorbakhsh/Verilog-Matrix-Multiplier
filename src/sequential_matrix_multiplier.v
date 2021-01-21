17c6c1592605b997804e60855815b58d74ecba79
ahmadsalimi <ahsa9978@gmail.com>
2021-01-21 18:57:14 +0330

    change sequential interface

--- src/sequential_matrix_multiplier.v
+++ src/sequential_matrix_multiplier.v
@@ -3,32 +3,15 @@ module sequential_matrix_multiplier
     parameter m = 4
 )
 (
-    input_a,
-    input_b,
-    input_a_stb,
-    input_b_stb,
-    output_z_ack,
-    clk,
-    rst,
-    output_z,
-    output_z_stb,
-    input_a_ack,
-    input_b_ack
+    input [31:0]    input_a,
+    input [31:0]    input_b,
+    input           clk,
+    input           rst,
+    output [$ceil($clog2(m))-1:0]   a_i,
+    output [$ceil($clog2(m))-1:0]   a_j,
+    output [$ceil($clog2(m))-1:0]   b_i,
+    output [$ceil($clog2(m))-1:0]   b_j,
+    output [31:0]   output_z,
 );
-
-  input     clk;
-  input     rst;
-
-  input     [31:0] input_a  [m-1:0][m-1:0];
-  input     input_a_stb;
-  output    input_a_ack;
-
-  input     [31:0] input_b  [m-1:0][m-1:0];
-  input     input_b_stb;
-  output    input_b_ack;
-
-  output    [31:0] output_z [m-1:0][m-1:0];
-  output    output_z_stb;
-  input     output_z_ack;
 
 endmodule
