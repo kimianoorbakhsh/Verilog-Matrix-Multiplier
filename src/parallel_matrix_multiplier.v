55b9fe1b437c60f3aee3eb65319fb70af56c9ddc
ahmadsalimi <ahsa9978@gmail.com>
2021-01-21 17:58:05 +0330

    generate sequential multiplier

--- src/parallel_matrix_multiplier.v
+++ src/parallel_matrix_multiplier.v
@@ -20,4 +20,33 @@ module parallel_matrix_multiplier
     output  [31:0]                  z_out
 );
 
+localparam seq_num = $ceil(n / m);
+localparam index_width = $ceil($clog2(m));
+
+reg [31:0]  A       [0:n-1][0:n-1];
+reg [31:0]  B       [0:n-1][0:n-1];
+reg [31:0]  seq_res [0:seq_num-1][0:seq_num-1][0:m-1][0:m-1];
+
+genvar i, j;
+generate
+    for (i = 0; i < seq_num; i = i + 1) begin
+        for (j = 0; j < seq_num; j = j + 1) begin
+            sequential_matrix_multiplier SEQ (
+                .clk(clk),
+                .rst(rst),
+                .a_in(A[i * m + a_i][j * m + a_j]),
+                .b_in(B[i * m + b_i][j * m + b_j]),
+                .a_i(a_i),
+                .a_j(a_j),
+                .b_i(b_i),
+                .b_j(b_j),
+                .z_out(seq_res[i][j][z_i][z_j]),
+                .z_i(z_i),
+                .z_j(z_j),
+                .z_stb(z_stb)
+            );
+        end
+    end
+endgenerate
+
 endmodule
