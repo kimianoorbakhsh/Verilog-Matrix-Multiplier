c7efe8e4acd131a1d135b53c89f1c2d988c1d0b0
Saee Saadat <saee_saadat@outlook.com>
2021-02-04 17:02:36 +0330

    seems to be working juuuuust fine
    Co-authored-by: Ahmad Salimi <ahsa9978@gmail.com>
    Co-authored-by: Kimia Noorbakhsh <kimianoorbakhsh@gmail.com>
    Co-authored-by: doctorhoseinpour <alirezzzhp1378@gmail.com>

--- src/sequential_matrix_multiplier.v
+++ src/sequential_matrix_multiplier.v
@@ -1,6 +1,7 @@
 module sequential_matrix_multiplier
 #(
-    parameter m = 4
+    parameter m = 4,
+    parameter m_len = $ceil($clog2(m))
 )
 (
     input                           clk,
@@ -9,19 +10,13 @@ module sequential_matrix_multiplier
     input   [31:0]                  a_in,
     input   [31:0]                  b_in,
     input                           z_ack,
-    output  [4:0]  a_i,
-    output  [4:0]  a_j,
-    output  [4:0]  b_i,
-    output  [4:0]  b_j,
-    // output  [$ceil($clog2(m))-1:0]  a_i,
-    // output  [$ceil($clog2(m))-1:0]  a_j,
-    // output  [$ceil($clog2(m))-1:0]  b_i,
-    // output  [$ceil($clog2(m))-1:0]  b_j,
+    output  [m_len-1:0]             a_i,
+    output  [m_len-1:0]             a_j,
+    output  [m_len-1:0]             b_i,
+    output  [m_len-1:0]             b_j,
     output  reg [31:0]              z_out,
-    // output  [$ceil($clog2(m))-1:0]  z_j,
-    // output  [$ceil($clog2(m))-1:0]  z_i,
-    output  [4:0]  z_j,
-    output  [4:0]  z_i,
+    output  [m_len-1:0]             z_j,
+    output  [m_len-1:0]             z_i,
     output  reg                     z_stb,
     output  reg                     done
 );
